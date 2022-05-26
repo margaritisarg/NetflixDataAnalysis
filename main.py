eb208c2c128046ead7c9234a80531f61f10310c1
Mar <margaritis1argyrou@gmail.com>
2022-05-26 17:05:24 +0100

    play around with this

--- main.py
+++ main.py
@@ -4,11 +4,30 @@ import pandas as pd
 
 df = pd.read_csv('netflix_dataset/titles.csv')
 df2 = df
-df2 = df2.dropna()
+#df2 = df2.dropna()
 #print(df.sort_values(by="imdb_score", ascending="False"))
 
-hey = pd.pivot_table(df2, values = 'imdb_score', index=['title','genres', 'imdb_votes'], columns = 'type').reset_index()
-print(hey)
+#print(df2.columns)
+
+#print(df2.head(50))
+
+#hey = pd.pivot_table(df2, values = 'imdb_score', index=['title','genres', 'imdb_votes'], columns = 'type').reset_index()
+#print(hey)
+
+df3 = df2.loc[(df2["imdb_score"] >= 9) & (df2["type"] == "SHOW") & (df2["imdb_votes"] > 100) ] 
+#df4 = df3.loc[df3["genres"] == "animation"]
+#df3_not_include = df3.loc[~df3["title"].str.contains('Breaking')]
+
+df4 = (df3.loc[:, ["title", "type", "imdb_score", "genres", "seasons", "imdb_votes"]]).sort_values("imdb_votes", ascending=False).head(10)
+print( (df3.loc[:, ["title", "type", "imdb_score", "genres", "seasons", "imdb_votes"]]).sort_values("imdb_votes", ascending=False).head(10) )
+
+
+series = (df4.loc[:, "genres"])
+#for index, array in series.items():
+#    print(f"{index}: {array}")
+
+#print( series.loc["drama"] )
+#print(len(df3.head(10)))
 
 #print( df2.loc[:, ["title", "type", "genres",  "release_year", "runtime", "imdb_score", "imdb_votes"]] )
 
